537ec47a47dbd25424755db9d09f204f3824603e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-16 20:45:30 +0300

    feat: import genDiff, add action and call function genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from "../src/index.js";
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filePath1, filePath2) => {
+    genDiff(filePath1, filePath2);
+  });
 
 program.parse();
