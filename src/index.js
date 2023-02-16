a2fac43b56fbd7e128cd95775c09dbb301b71500
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-16 20:38:55 +0300

    feat: create function readFile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  return fs.readFileSync(fullPath).toString();
+};
+
 const genDiff = (filePath1, filePath2) => {
 
 };
