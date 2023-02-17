93dbea9910ee684d42c20389b75bab6a0a9659aa
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-17 22:38:49 +0300

    feat: import lodash and create function compareData

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import path from 'path';
 import fs from 'fs';
+import _ from 'lodash';
 
 const readFile = (filePath) => {
   const fullPath = path.resolve(process.cwd(), filePath);
   return fs.readFileSync(fullPath).toString();
+};
+
+const compareData = (dataOfFirstFile, dataOfSecondFile) => {
+  const keys = Object.keys({...dataOfFirstFile, ...dataOfSecondFile});
+  const sortedKeys = _.sortBy(keys);
+  let diff = '';
+
+  sortedKeys.forEach(key => {
+    if (Object.hasOwn(dataOfFirstFile, key) && (Object.hasOwn(dataOfSecondFile, key)) && dataOfFirstFile[key] === dataOfSecondFile[key]) {
+      diff += `\n    ${key}: ${dataOfFirstFile[key]}\n`;
+    }
+
+    if (Object.hasOwn(dataOfFirstFile, key) && (Object.hasOwn(dataOfSecondFile, key)) && dataOfFirstFile[key] !== dataOfSecondFile[key]) {
+      diff += `\n  - ${key}: ${dataOfFirstFile[key]}\n`;
+      diff += `\n  + ${key}: ${dataOfSecondFile[key]}\n`;
+    }
+
+    if (Object.hasOwn(dataOfFirstFile, key) && !(Object.hasOwn(dataOfSecondFile, key))) {
+      diff += `\n  - ${key}: ${dataOfFirstFile[key]}\n`;
+    }
+
+    if (!Object.hasOwn(dataOfFirstFile, key) && (Object.hasOwn(dataOfSecondFile, key))) {
+      diff += `\n  + ${key}: ${dataOfSecondFile[key]}\n`;
+    }
+  });
+
+  return `{${diff}}`;
 };
 
 const genDiff = (filePath1, filePath2) => {
