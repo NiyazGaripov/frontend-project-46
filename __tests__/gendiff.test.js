99f971d93317e4143e58000f0615d27e3bb91675
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-21 22:08:02 +0300

    feat: create constants __filename and __dirname

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,3 +3,6 @@ import path, { dirname } from 'path';
 import fs from 'fs';
 import { test, expect } from '@jest/globals';
 import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
