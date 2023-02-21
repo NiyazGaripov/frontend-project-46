8b56cf5e370ca164a242c60992c42f8db01da3cc
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-21 22:08:38 +0300

    feat: create functions getFixturePath and readFile

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,3 +6,6 @@ import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename));
