12ada94ffe4d259733382ad323816dd8ad6ce36f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-23 21:24:24 +0300

    feat: create constants __filename, __dirname and functions getFixturePath, readFile

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -4,3 +4,9 @@ import fs from 'fs';
 import { expect, test } from '@jest/globals';
 import yaml from 'js-yaml';
 import parse from '../src/parsers.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename));
