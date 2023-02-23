deb53bca2658a75e033eb72b7910e0eabf18eed8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-02-23 14:08:05 +0300

    feat: add test for compare yaml files and json and yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,12 +10,16 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename));
 
-const filePath1 = getFixturePath('file1.json');
-const filePath2 = getFixturePath('file2.json');
+const jsonFilePath1 = getFixturePath('file1.json');
+const jsonFilePath2 = getFixturePath('file2.json');
+const yamlFilePath1 = getFixturePath('file1.yml');
+const yamlFilePath2 = getFixturePath('file2.yml');
 const expected = readFile('expected.txt').toString().trim();
 
 test('should to get diff between two files', () => {
-  expect(genDiff(filePath1, filePath2)).toStrictEqual(expected);
+  expect(genDiff(jsonFilePath1, jsonFilePath2)).toStrictEqual(expected);
+  expect(genDiff(yamlFilePath1, yamlFilePath2)).toStrictEqual(expected);
+  expect(genDiff(jsonFilePath1, yamlFilePath2)).toStrictEqual(expected);
 });
 
 test('should to get exception', () => {
