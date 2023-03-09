bdacd7884fb43b076a255ab2c9d2fdb1627e9393
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-09 09:02:20 +0300

    feat: add stylish as default formatter, get format from opts

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,10 +6,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filePath1, filePath2) => {
-    console.log(genDiff(filePath1, filePath2));
+    const format = `${program.opts().format}`;
+    console.log(genDiff(filePath1, filePath2, format));
   });
 
 program.parse();
