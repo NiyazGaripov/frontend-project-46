d05a22891e687226ec894dd69f33f7f10242ca42
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-18 14:23:49 +0300

    feat: implement function getStringPlainFormat

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,3 +1,23 @@
-const getStringPlainFormat = (tree) => {};
+const getStringPlainFormat = (tree) => {
+  const iter = (node, path = '') => node.reduce((lines, { nodeKey, nodeValue, nodeState }) => {
+    const propertyPath = createPropertyPath(path, nodeKey);
+
+    if (Array.isArray(nodeValue)) {
+      return [...lines, ...getUpdatedStructure(propertyPath, nodeValue).filter((element) => element !== '')];
+    }
+
+    if (nodeState === '-') {
+      return [...lines, getTextWithRemovedProperty(nodeKey)];
+    }
+
+    if (nodeState === '+') {
+      return [...lines, getTextWithAddedProperty(nodeKey, transformValue(nodeValue))];
+    }
+
+    return [...lines, ...iter(nodeValue, `${propertyPath}.${nodeKey}`)];
+  }, []);
+
+  return iter(tree).filter((item) => item !== '').join('\n');
+};
 
 export default getStringPlainFormat;
