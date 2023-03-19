1543b71e4ddb1c0f6cd209a0b9c57def54c49d0a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-19 20:36:17 +0300

    refactor: create constant DEFAULT_INDENT, add condotions, rename key, value and state

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,16 +1,18 @@
 import _ from 'lodash';
 
+const DEFAULT_INDENT = 2;
+
 const stringify = (node, depth) => {
   if (!_.isObject(node)) {
     return `${node}`;
   }
 
-  const indentSize = depth * 2;
-  const currentIndent = ' '.repeat(indentSize + 2);
-  const bracketIndent = ' '.repeat(indentSize - 2);
+  const indentSize = depth * DEFAULT_INDENT;
+  const currentIndent = ' '.repeat(indentSize + DEFAULT_INDENT);
+  const bracketIndent = ' '.repeat(indentSize - DEFAULT_INDENT);
   const lines = Object
     .entries(node)
-    .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + 2)}`);
+    .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + DEFAULT_INDENT)}`);
 
   return ['{', ...lines, `${bracketIndent}}`].join('\n');
 };
@@ -21,20 +23,20 @@ const getStringStylishFormat = (tree) => {
       return `${node}`;
     }
 
-    const indentSize = depth * 2;
+    const indentSize = depth * DEFAULT_INDENT;
     const currentIndent = ' '.repeat(indentSize);
-    const bracketIndent = ' '.repeat(indentSize - 2);
+    const bracketIndent = ' '.repeat(indentSize - DEFAULT_INDENT);
 
     if (!_.isArray(node)) {
       return stringify(node, depth);
     }
 
-    const lines = node.map(({ key, value, state }) => {
-      if (String(value).length) {
-        return `${currentIndent}${state} ${key}: ${iter(value, depth + 2)}`;
+    const lines = node.map(({ nodeKey, nodeValue, nodeState }) => {
+      if (String(nodeValue).length) {
+        return `${currentIndent}${nodeState} ${nodeKey}: ${iter(nodeValue, depth + DEFAULT_INDENT)}`;
       }
 
-      return `${currentIndent}${state} ${key}:`;
+      return `${currentIndent}${nodeState} ${nodeKey}:`;
     });
 
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
