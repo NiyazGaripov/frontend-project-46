9e6b0f6d8b5ac318f05cf8b0c3af5d82d90fd890
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-24 16:31:52 +0300

    refactor: rename constants

--- src/ast.js
+++ src/ast.js
@@ -2,31 +2,31 @@ import _ from 'lodash';
 
 const buildAST = (data1, data2) => _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
   .map((key) => {
-    const valueFromFirstFile = data1[key];
-    const valueFromSecondFile = data2[key];
+    const valueFromData1 = data1[key];
+    const valueFromData2 = data2[key];
 
     if (!_.has(data2, key)) {
-      return { key, newValue: valueFromFirstFile, type: 'removed' };
+      return { key, newValue: valueFromData1, type: 'removed' };
     }
 
     if (!_.has(data1, key)) {
-      return { key, newValue: valueFromSecondFile, type: 'added' };
+      return { key, newValue: valueFromData2, type: 'added' };
     }
 
     if (
-      (!_.isObject(valueFromFirstFile) || !_.isObject(valueFromSecondFile))
-      && valueFromFirstFile !== valueFromSecondFile) {
+      (!_.isObject(valueFromData1) || !_.isObject(valueFromData2))
+      && valueFromData1 !== valueFromData2) {
       return {
-        key, oldValue: valueFromFirstFile, newValue: valueFromSecondFile, type: 'updated',
+        key, oldValue: valueFromData1, newValue: valueFromData2, type: 'updated',
       };
     }
 
-    if (_.isObject(valueFromFirstFile) && _.isObject(valueFromSecondFile)) {
-      const children = buildAST(valueFromFirstFile, valueFromSecondFile);
+    if (_.isObject(valueFromData1) && _.isObject(valueFromData2)) {
+      const children = buildAST(valueFromData1, valueFromData2);
       return { key, children, type: 'nested' };
     }
 
-    return { key, newValue: valueFromFirstFile, type: 'unchanged' };
+    return { key, newValue: valueFromData1, type: 'unchanged' };
   });
 
 export default buildAST;
