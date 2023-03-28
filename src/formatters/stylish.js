1bc425f7d96a88a4e60ca51c8b129356843da6ea
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-28 22:25:16 +0300

    refactor: remove unused code, create function makeIndent, correct logic indent build

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,52 +1,62 @@
 import _ from 'lodash';
 
-const DEFAULT_INDENT = 2;
+const makeIndent = (depth, hasOffsetToLeft = false, replacer = ' ') => {
+  const INDENT_COUNT = 4;
+  const OFFSET_TO_LEFT = 2;
+  const indentSize = depth * INDENT_COUNT;
+
+  return hasOffsetToLeft ? replacer.repeat(indentSize - OFFSET_TO_LEFT) : replacer.repeat(indentSize);
+};
 
 const stringify = (node, depth) => {
   if (!_.isPlainObject(node)) {
     return `${node}`;
   }
 
-  const indentSize = depth * DEFAULT_INDENT;
-  const currentIndent = ' '.repeat(indentSize + DEFAULT_INDENT);
-  const bracketIndent = ' '.repeat(indentSize - DEFAULT_INDENT);
+  const closeBracketIndent = makeIndent(depth);
+
   const lines = Object
     .entries(node)
-    .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + DEFAULT_INDENT)}`);
+    .map(([key, value]) => {
+      const indent = makeIndent(depth + 1);
 
-  return ['{', ...lines, `${bracketIndent}}`].join('\n');
+      return `${indent}${key}: ${stringify(value, depth + 1)}`;
+    });
+
+  return ['{', ...lines, `${closeBracketIndent}}`].join('\n');
 };
 
 const getStringStylishFormat = (tree) => {
-  const iter = (node, depth) => {
-    const indentSize = depth * DEFAULT_INDENT;
-    const currentIndent = ' '.repeat(indentSize);
-    const bracketIndent = ' '.repeat(indentSize - DEFAULT_INDENT);
+  const iter = (node, depth = 0) => {
+    const closeBracketIndent = makeIndent(depth);
 
     const lines = node.map(({
       key, value1, value2, nodeValue, children, type,
     }) => {
+      const indentWithOffset = makeIndent(depth + 1, true);
+      const indent = makeIndent(depth + 1);
+
       switch (type) {
         case 'removed':
-          return `${currentIndent}- ${key}: ${stringify(nodeValue, depth + DEFAULT_INDENT)}`;
+          return `${indentWithOffset}- ${key}: ${stringify(nodeValue, depth + 1)}`;
         case 'added':
-          return `${currentIndent}+ ${key}: ${stringify(nodeValue, depth + DEFAULT_INDENT)}`;
+          return `${indentWithOffset}+ ${key}: ${stringify(nodeValue, depth + 1)}`;
         case 'updated': {
-          const removedData = `${currentIndent}- ${key}: ${stringify(value1, depth + DEFAULT_INDENT)}`;
-          const addedData = `${currentIndent}+ ${key}: ${stringify(value2, depth + DEFAULT_INDENT)}`;
+          const removedData = `${indentWithOffset}- ${key}: ${stringify(value1, depth + 1)}`;
+          const addedData = `${indentWithOffset}+ ${key}: ${stringify(value2, depth + 1)}`;
           return `${removedData}\n${addedData}`;
         }
         case 'nested':
-          return `${currentIndent}  ${key}: ${iter(children, depth + DEFAULT_INDENT)}`;
+          return `${indent}${key}: ${iter(children, depth + 1)}`;
         default:
-          return `${currentIndent}  ${key}: ${stringify(nodeValue, depth + DEFAULT_INDENT)}`;
+          return `${indent}${key}: ${stringify(nodeValue, depth + 1)}`;
       }
     });
 
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+    return ['{', ...lines, `${closeBracketIndent}}`].join('\n');
   };
 
-  return iter(tree, 1);
+  return iter(tree);
 };
 
 export default getStringStylishFormat;
